2784eb38dc43ec1bff45b928bbb258575e0fd4aa
ifatmagamha <you@example.com>
2023-09-27 09:42:10 +0100

    Correction du port d'écoute et modification de myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`useful prgrammer info security App started on port ${PORT}`);
+});
 
 
 
